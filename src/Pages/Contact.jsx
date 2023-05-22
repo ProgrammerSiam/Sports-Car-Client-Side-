5b9701a4bcfb1169f4598f3769d4f4b2ed3b5337
Siam10101 <mehedihasansiam030@gmail.com>
2023-05-22 18:36:41 +0600

    complete a contact section

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -7,13 +7,6 @@ export default function Contact() {
         contact <span>us</span>
       </h1>
       <div className="row">
-        {/* <iframe
-          className="map"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30153.788252261566!2d72.82321484621745!3d19.141690214227783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b63aceef0c69%3A0x2aa80cf2287dfa3b!2sJogeshwari%20West%2C%20Mumbai%2C%20Maharashtra%20400047!5e0!3m2!1sen!2sin!4v1632137920043!5m2!1sen!2sin"
-          allowFullScreen
-          loading="lazy"
-        /> */}
-
         <iframe
           className="map"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d116651.08772517763!2d89.19614339322331!3d23.983538692534644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fe84f8fd9ccc71%3A0xbad939f3f5ee0c50!2sPabna%20Sadar%20Upazila!5e0!3m2!1sen!2sbd!4v1684559033534!5m2!1sen!2sbd"
